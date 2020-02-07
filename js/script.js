0d17c6cb05cb20def640094ad3af8c6dde8e1df9
design4people <51137179+design4people@users.noreply.github.com>
2020-02-07 10:52:32 +0200

    submenu opening scripts
    submenu opening scripts in the mobile version of the site
    скрипти відкривання підменю в мобільній версії сайту

--- js/script.js
+++ js/script.js
@@ -12,3 +12,32 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+
+var ItemSublist21 = document.querySelector('.item-sublist21');
+var navSublist21Toggle = document.querySelector('.nav-sublist21__toggle');
+
+navSublist21Toggle.addEventListener('click', function() {
+  if (ItemSublist21.classList.contains('nav-sublist21--closed')) {
+    ItemSublist21.classList.remove('nav-sublist21--closed');
+    ItemSublist21.classList.add('nav-sublist21--opened');
+  } else {
+    ItemSublist21.classList.add('nav-sublist21--closed');
+    ItemSublist21.classList.remove('nav-sublist21--opened');
+  }
+});
+
+
+var ItemSublist22 = document.querySelector('.item-sublist22');
+var navSublist22Toggle = document.querySelector('.nav-sublist22__toggle');
+
+navSublist22Toggle.addEventListener('click', function() {
+  if (ItemSublist22.classList.contains('nav-sublist22--closed')) {
+    ItemSublist22.classList.remove('nav-sublist22--closed');
+    ItemSublist22.classList.add('nav-sublist22--opened');
+  } else {
+    ItemSublist22.classList.add('nav-sublist22--closed');
+    ItemSublist22.classList.remove('nav-sublist22--opened');
+  }
+});
+
