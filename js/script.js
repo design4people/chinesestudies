f95fc0953564c7b0fd8645b6cb8efddb15020ce1
design4people <51137179+design4people@users.noreply.github.com>
2020-03-02 13:16:44 +0200

    change classnames of main navigation list control

--- js/script.js
+++ js/script.js
@@ -14,30 +14,30 @@ navToggle.addEventListener('click', function() {
 });
 
 
-var ItemSublist21 = document.querySelector('.item-sublist21');
-var navSublist21Toggle = document.querySelector('.nav-sublist21__toggle');
+var sublistArhive = document.querySelector('.sublist-arhive');
+var arhiveToggle = document.querySelector('.sublist-arhive__toggle');
 
-navSublist21Toggle.addEventListener('click', function() {
-  if (ItemSublist21.classList.contains('nav-sublist21--closed')) {
-    ItemSublist21.classList.remove('nav-sublist21--closed');
-    ItemSublist21.classList.add('nav-sublist21--opened');
+arhiveToggle.addEventListener('click', function() {
+  if (sublistArhive.classList.contains('sublist-arhive--closed')) {
+    sublistArhive.classList.remove('sublist-arhive--closed');
+    sublistArhive.classList.add('sublist-arhive--opened');
   } else {
-    ItemSublist21.classList.add('nav-sublist21--closed');
-    ItemSublist21.classList.remove('nav-sublist21--opened');
+    sublistArhive.classList.add('sublist-arhive--closed');
+    sublistArhive.classList.remove('sublist-arhive--opened');
   }
 });
 
 
-var ItemSublist22 = document.querySelector('.item-sublist22');
-var navSublist22Toggle = document.querySelector('.nav-sublist22__toggle');
+var sublistForauthors = document.querySelector('.sublist-forauthors');
+var forauthorsToggle = document.querySelector('.sublist-forauthors__toggle');
 
-navSublist22Toggle.addEventListener('click', function() {
-  if (ItemSublist22.classList.contains('nav-sublist22--closed')) {
-    ItemSublist22.classList.remove('nav-sublist22--closed');
-    ItemSublist22.classList.add('nav-sublist22--opened');
+forauthorsToggle.addEventListener('click', function() {
+  if (sublistForauthors.classList.contains('sublist-forauthors--closed')) {
+    sublistForauthors.classList.remove('sublist-forauthors--closed');
+    sublistForauthors.classList.add('sublist-forauthors--opened');
   } else {
-    ItemSublist22.classList.add('nav-sublist22--closed');
-    ItemSublist22.classList.remove('nav-sublist22--opened');
+    sublistForauthors.classList.add('sublist-forauthors--closed');
+    sublistForauthors.classList.remove('sublist-forauthors--opened');
   }
 });
 
